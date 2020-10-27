239f88138fa217eff35deeff6187ff3d4951371b
slashkeyvalue <59461439+slashkeyvalue@users.noreply.github.com>
2020-10-27 02:10:35 -0300

    tweak(cfg): sql stuff

--- vrp/cfg/base.lua
+++ vrp/cfg/base.lua
@@ -3,9 +3,9 @@ local cfg = {}
 cfg.db = {
 	driver = "ghmattimysql",
 	host = "127.0.0.1",
-	database = "DATABASEMYSQL",
-	user = "LOGINMYSQL",
-	password = "SENHAMYSQL"
+	database = "vrpex",
+	user = "root",
+	password = ""
 }
 
 return cfg
